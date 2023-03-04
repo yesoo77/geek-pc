426a99d60038d51b44a2c01d550b4339283fc4ba
itheima <itheima@itcast.cn>
2023-03-04 20:24:02 +0800

    项目搭建-配置路由

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
-import logo from './logo.svg'
-import './App.css'
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom'
+import Home from './pages/Layout/index'
+import Login from './pages/Login/index'
 
 function App() {
-  return <div className="App"></div>
+  return (
+    <Router>
+      <div className="App">
+        <Link to="/login">登录</Link>
+        <Link to="/home">首页</Link>
+        {/* 配置路由的规则 */}
+        <Switch>
+          <Route path="/home" component={Home}></Route>
+          <Route path="/login" component={Login}></Route>
+          {/* 配置一个404组件 */}
+        </Switch>
+      </div>
+    </Router>
+  )
 }
 
 export default App
